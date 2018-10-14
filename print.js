d31b6864aab111f256c607c028d72cff96e781de
jsonchou <jsonchou@outlook.com>
2018-10-14 19:56:06 +0800

    fix pages

--- print.js
+++ print.js
@@ -3,26 +3,45 @@ const request = require('request-promise')
 const sqlite = require('sqlite')
 const rainbow = require('done-rainbow')
 
-const dir = './.tmp/pv6.txt'
-
-if (!fs.existsSync(dir)) {
-    fs.createFileSync(dir)
-}
-
+let base = 10000;
+let index = 1;
 
 let dox = async () => {
     const db = await sqlite.open('E:/perfect-last/Data/1587/SpiderResult.db3');
 
-    // let keyArrs = await db.all(`SELECT pv_key FROM Content where ID<8000000 LIMIT 10`)
-    let keyArrs = await db.all(`SELECT pv_key FROM Content where ID >18000000 and ID<=22000000 and pv_key !='' `)
+    let _evt = async () => {
 
-    let proms = keyArrs.map(item => {
-        fs.appendFileSync(dir, `https://baike.baidu.com/api/lemmapv?id=${item.pv_key}&r=${Date.now()}\r\n`)
-    })
+        let dir = './.tmp/index.txt'
 
-    let sqlArrs = await Promise.all(proms);
+        if (!fs.existsSync(dir)) {
+            fs.createFileSync(dir)
+        }
 
-    rainbow('done!')
+        let keyArrs = await db.all(`SELECT pv_key FROM Content where ID >${(index-1)*base} and ID<=${index*base} and pv_key !='' `)
+
+        let proms = keyArrs.map(item => {
+            fs.appendFileSync(dir, `<a href="//baike.baidu.com/api/lemmapv?id=${item.pv_key}&r=${new Date().getMinutes()}"></a>\r\n`)
+        })
+
+        let sqlArrs = await Promise.all(proms);
+
+        index += 1;
+
+        rainbow(` ${index} done!`)
+
+        if (index >= 3) {
+            rainbow('all done!')
+            process.exit();
+            return;
+        }
+
+        await _evt();
+
+    }
+
+    await _evt();
+
+    rainbow('all done!')
 
 };
 
